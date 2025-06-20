4442e2bb05270e5f1cc418db9dfd649c2f95741e
Dipendra Sharma <dipendra.sharma@apnamart.in>
2025-06-20 15:39:20 +0530

    default

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,11 @@ deviceManager.onDeviceDisconnect((d)=>{
 
 deviceManager.setDeviceConfig('0x26f1', '0x5650', {deviceType: 'scanner', setToDefault: true, baudrate: 9600, model: 'Table Top PD-310', brand: 'balajiPOS'}).then()
 deviceManager.setDeviceConfig('0x67b', '0x23a3', {deviceType: 'scale', setToDefault: true, baudrate: 9600, model: 'DS-252', brand: 'Essae'}).then()
+deviceManager.setDeviceConfig('0x483', '0x5743', {deviceType: 'printer', setToDefault: true, baudrate: "not-supported", model: 'RP-803', brand: 'balajiPOS'}).then()
 
 scannerManager.onScanData((d)=>{
 	console.log(`Scan data ${JSON.stringify(d)}`);
+	printerManager.printToDefault('Scan data: ' + JSON.stringify(d) + '')
 })
 
 scaleManager.onWeightData((data)=>{
